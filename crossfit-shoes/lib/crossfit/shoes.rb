eed95366cb2d64b3f336e96b43c3c6f923c0ea87
Katie Wilday <wildayk@gmail.com>
2021-02-06 14:20:00 +0000

    Automatically backed up by Learn

---
+++ crossfit-shoes/lib/crossfit/shoes.rb
@@ -0,0 +1,8 @@
+require "crossfit/shoes/version"
+
+module Crossfit
+  module Shoes
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ crossfit-shoes/lib/crossfit/shoes/version.rb
@@ -0,0 +1,5 @@
+module Crossfit
+  module Shoes
+    VERSION = "0.1.0"
+  end
+end
